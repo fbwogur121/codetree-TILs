bab737aab464fceaea3a67c1ad4f65c170cd1f12
Rjhkki222 <65850233+fbwogur121@users.noreply.github.com>
2024-01-26 13:39:54 +0900

    Title: 최고의 33위치, Time: 116ms, Memory: 25MB, Status: Passed - CodeTree

--- 240126/최고의 33위치/best-place-of-33.py
+++ 240126/최고의 33위치/best-place-of-33.py
@@ -5,8 +5,8 @@ max = 0
 for i in range(a):
     for j in range(a):
         max_now = 0
-        for a in range(3):
-            for b in range(3):
-                max_now += arr[i+a][j+b]
+        for k in range(3):
+            for l in range(3):
+                max_now += arr[i+k][j+l]
         if max_now >= max: max = max_now
 print(max)
